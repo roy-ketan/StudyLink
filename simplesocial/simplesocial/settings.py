045b12e6881ba86bfbaef5d5233202a89f91ad7e
Ketan <ketanroy0866@gmail.com>
2024-07-03 11:15:31 +0530

    settings

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -23,9 +23,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = 't&=eb=%y!h_w-nd#jzf**x7r6i3@*k7xapx5a5zhqy-mscsq1x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
