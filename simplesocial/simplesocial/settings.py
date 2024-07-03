3651153754ec479dd74bfa381dd6f37f3c5a5107
Ketan <ketanroy0866@gmail.com>
2024-07-03 14:11:59 +0530

    heroku

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -123,7 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles'),
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+django_heroku.settings(locals())
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
